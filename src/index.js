57dc70b8d708b7ee85443d992f8d4b7ab46ab197
Tolugaa <guitoluga@gmail.com>
2025-01-17 16:13:36 -0300

    declarando variaveis e estrutura de decisao

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 let nome = "guilherme";
-console.log(nome);
+let exp = 10000;
+let rank = "";
+if (exp<1000){
+    rank = "Ferro";
+}
+else if (exp<2000){
+    rank = "Bronze";
+}
+else if (exp<3000){
+    rank = "Prata";
+}
+else if (exp<4000){
+    rank = "Ouro";
+}
+else if (exp<5000){
+    rank = "Platina";
+}
+else if (exp<6000){
+    rank = "Diamante";
+}
+else if (exp<7000){
+    rank = "Ascendente";
+}
+else if (exp<8000){
+    rank = "mestre";
+}
+else if (exp<10000){
+    rank = "Imortal";
+}
+else {
+    rank = "Radiante";
+}
+
+console.log(nome + " você está no rank " + rank);
